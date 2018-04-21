be304daebb76f08a1e65c5308433d97c05dbea94
Avelino Javer <avelinojaver@avelinos-mbp.home>
2018-04-21 09:33:07 +0100

    .

--- train.py
+++ train.py
@@ -6,9 +6,7 @@ Created on Tue Nov 21 11:42:24 2017
 @author: ajaver
 """
 
-import math
 import tqdm
-import numpy as np
 import torch
 from torch import nn
 
@@ -18,31 +16,24 @@ from unet import UNet, _crop
 from torch.utils.data import Dataset, DataLoader
 
 class AugmentedDataset(ImgFlowSplitted, Dataset):
-    def __init__(self, cuda_id = -1, **argkws):
+    def __init__(self, **argkws):
         ImgFlowSplitted.__init__(self, **argkws)
-        self.cuda_id = cuda_id
         
     def __getitem__(self, index):
         dat = ImgFlowSplitted.__getitem__(self, index)
-        dat = tuple(self._prepare_for_torch(x) for x in dat)
+        dat = tuple(torch.from_numpy(x[None, ...]) for x in dat)
         return dat
-        
-    
-    def _prepare_for_torch(self, X):
-        X = torch.from_numpy(X[None, ...])
-        
-        if self.cuda_id >= 0:
-            X = X.cuda(self.cuda_id)
-        
-        #X = torch.autograd.Variable(X)
-        return X
-    
     
     def __len__(self):
         return ImgFlowSplitted.__len__(self)
     
+    
+def _prepare_for_torch(X, cuda_id):
+    if cuda_id >= 0:
+        X = X.cuda(cuda_id)
+    X = torch.autograd.Variable(X)
+    return X
 
-#%%
 
 if __name__ == '__main__':
     cuda_id = -1
@@ -51,8 +42,8 @@ if __name__ == '__main__':
         cuda_id = 0
         
     n_epochs = 3
-    batch_size = 16
-    num_workers = 8
+    batch_size = 4
+    num_workers = 2
     
     gen_imgs = AugmentedDataset(test_split = 0.1,
                                pad_size = 32,
@@ -74,8 +65,8 @@ if __name__ == '__main__':
     for epoch in range(n_epochs):
         pbar = tqdm.tqdm(gen)
         for X, target in pbar:
-            X = torch.autograd.Variable(X)
-            target = torch.autograd.Variable(target)
+            X = _prepare_for_torch(X, cuda_id)
+            target =  _prepare_for_torch(target, cuda_id)
             
             pred = model(X)
             target_cropped = _crop(pred, target)
